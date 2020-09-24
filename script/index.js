2eec4b8a1c0e18ad00f876838241e763a6f4dc54
sadick254 <sadickjunior@gmail.com>
2020-09-25 01:38:34 +0300

    Try another terminal color thing

--- script/index.js
+++ script/index.js
@@ -1,7 +1,6 @@
 const yargs = require('yargs')
+const chalk = require('chalk')
 
 const options = yargs(process.argv)
 
-options.alias('v', 'version').version('2')
-
-console.log(options.argv)
+options.alias('v', 'version').version(chalk.blue('@latest')).argv
